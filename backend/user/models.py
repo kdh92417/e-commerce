c1b665ef4174287c4e95313cdebfd9dbd555a8fe
kdh92417 <kdh92417@gmail.com>
2022-09-09 18:04:16 +0900

    :sparkles: Feat: 공통 타임 모델 추가
    타임 추상모델 추가
    - 컬럼 : updated_at, created_at

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from backend.core.models import TimeStampModel
+
+
+class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
+    """유저 모델"""
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(_("username"), max_length=45, validators=[username_validator], null=True)
+    email = models.EmailField(_("email_address"), unique=True)
+    is_active = models.BooleanField(_("staff status"), default=False)
+
+    object = UserManager()
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        db_table = "users"
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
