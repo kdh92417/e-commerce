01ee265c14391602773fdd74ad69d3dc91fddf6a
kdh92417 <kdh92417@gmail.com>
2022-09-11 15:41:17 +0900

    Feat: 공통 ImageModel 추가
    추상 ImageModel 작성

---
+++ backend/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class TimeStampModel(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ImageModel(models.Model):
+    origin_name = models.CharField(max_length=300, null=False)
+    stored_name = models.CharField(max_length=300, null=False)
+    file_size = models.IntegerField(default=0, null=False, blank=False)
+
+    class Meta:
+        abstract = True
