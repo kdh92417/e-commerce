b021335a194982a7f075c1f05a9f7ffa31d90244
kdh92417 <kdh92417@gmail.com>
2022-09-11 15:43:44 +0900

    Style: black을 이용한 오토코드 포멧팅
    오토코드포멧팅으로 인한 코드 스타일 수정

--- backend/product/models.py
+++ backend/product/models.py
@@ -6,6 +6,7 @@ from core.models import TimeStampModel, ImageModel
 
 class Category(models.Model):
     """상품 카테고리 모델"""
+
     name = models.CharField(max_length=45)
 
     def __str__(self):
@@ -14,6 +15,7 @@ class Category(models.Model):
 
 class Product(TimeStampModel):
     """상품 모델"""
+
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
     stock = models.IntegerField(default=1)
@@ -30,8 +32,13 @@ class Product(TimeStampModel):
 
 class ProductReply(TimeStampModel):
     """상품 리뷰 모델"""
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=False, blank=False)
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=False, blank=False
+    )
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=False, blank=False
+    )
     content = models.TextField()
     ref_reply = models.IntegerField(default=None, null=True)
     type = models.CharField(max_length=45)
@@ -44,7 +51,10 @@ class ProductReply(TimeStampModel):
 
 class ProductImage(ImageModel, TimeStampModel):
     """상품 이미지 모델"""
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False)
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=False, blank=False
+    )
     is_thumbnail = models.BooleanField(default=False)
 
     class Meta:
@@ -56,7 +66,10 @@ class ProductImage(ImageModel, TimeStampModel):
 
 class ProductReplyImage(ImageModel, TimeStampModel):
     """상품 리뷰 이미지 모델"""
-    product_reply = models.ForeignKey(ProductReply, on_delete=models.CASCADE, null=False, blank=False)
+
+    product_reply = models.ForeignKey(
+        ProductReply, on_delete=models.CASCADE, null=False, blank=False
+    )
 
     class Meta:
         db_table = "product_reply_images"
@@ -67,8 +80,13 @@ class ProductReplyImage(ImageModel, TimeStampModel):
 
 class Like(models.Model):
     """상품 좋아요 모델"""
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=False, blank=False)
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=False, blank=False
+    )
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=False, blank=False
+    )
 
     class Meta:
         db_table = "likes"
