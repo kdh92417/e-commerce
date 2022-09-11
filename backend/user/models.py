4dd71ae35edb8c79e70d7513135ede6f14f84723
kdh92417 <kdh92417@gmail.com>
2022-09-11 15:43:02 +0900

    Feat: Cart 모델 작성
    장바구니 모델 작성

--- backend/user/models.py
+++ backend/user/models.py
@@ -4,14 +4,18 @@ from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import gettext_lazy as _
 
-from backend.core.models import TimeStampModel
+from core.models import TimeStampModel
+from product.models import Product
 
 
 class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
     """유저 모델"""
+
     username_validator = UnicodeUsernameValidator()
 
-    username = models.CharField(_("username"), max_length=45, validators=[username_validator], null=True)
+    username = models.CharField(
+        _("username"), max_length=45, validators=[username_validator], null=True
+    )
     email = models.EmailField(_("email_address"), unique=True)
     is_staff = models.BooleanField(_("Is staff"), default=False)
 
@@ -29,3 +33,16 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
         super().clean()
         self.email = self.__class__.objects.normalize_email(self.email)
 
+
+class Cart(models.Model):
+    """장바구니 모델"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=False, blank=False
+    )
+    quantity = models.IntegerField(default=1, null=False, blank=False)
+    product_price = models.IntegerField(default=0, null=False, blank=False)
+
+    class Meta:
+        db_table = "carts"
