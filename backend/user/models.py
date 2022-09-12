4731ead76745fa9440efebe40ccad90f7ac90fd6
kdh92417 <kdh92417@gmail.com>
2022-09-12 15:31:35 +0900

    Feat: 유저 기능 추가
    유저 기능 추가
    - 회원가입
    - 로그인
    - 로그아웃

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,11 +1,35 @@
-from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 from core.models import TimeStampModel
 from product.models import Product
+
+
+class UserManager(BaseUserManager):
+    """유저매니저 재정의"""
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        # extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
@@ -19,7 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
     email = models.EmailField(_("email_address"), unique=True)
     is_staff = models.BooleanField(_("Is staff"), default=False)
 
-    object = UserManager()
+    objects = UserManager()
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
     REQUIRED_FIELDS = []
