74b6480a279e8f30631d9f361ea8e827bb269a2c
kdh92417 <kdh92417@gmail.com>
2022-09-12 15:37:37 +0900

    Feat: ProductView API 작성
    제품 CRUD를 위한 ModelViewSet 작성

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.utils import json
+from rest_framework.views import APIView
 
-# Create your views here.
+from product.models import Product
+from product.serializers import ProductSerializer, ProductImageSerializer
+
+
+class ProductView(viewsets.ModelViewSet):
+    """
+    상품 조회, 생성, 수정, 삭제 API
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
