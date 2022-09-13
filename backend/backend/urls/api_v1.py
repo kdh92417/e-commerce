f8a5da2a6e6140849eeaf7571fcb8bc8a72bcee8
kdh92417 <kdh92417@gmail.com>
2022-09-13 09:24:17 +0900

    Feat: 시간및 유저 설정
    - 서울로 데이터베이스 시간설정
    - 기본 Auth User Model 설정

--- backend/backend/urls/api_v1.py
+++ backend/backend/urls/api_v1.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("users/", include("user.urls")),
-    path("product/", include("product.urls")),
+    path("products/", include("product.urls")),
 ]
