77fe0a716b7156b8f20e1a9ccdfa22a0b38306c8
kdh92417 <kdh92417@gmail.com>
2022-09-14 13:43:25 +0900

    Feat: order url 작성
    order url mapping

--- backend/backend/urls/api_v1.py
+++ backend/backend/urls/api_v1.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("users/", include("user.urls")),
     path("products/", include("product.urls")),
+    path("orders/", include("order.urls")),
 ]
