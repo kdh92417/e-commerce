149c36e546cf0072fd10262c97c1da6c97f23d0f
kdh92417 <kdh92417@gmail.com>
2022-09-14 15:30:23 +0900

    Fix: 결제 모델 수정
    payment_status 및 payment_method 컬럼 수정

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -10,15 +10,27 @@ PAYMENT_STATUS_CHOICES = [
     ("PW", "결재대기"),
 ]
 
+# 결제수단
+PAYMENT_METHODS_CHOICES = [
+    ("CC", "신용카드"),
+    ("DB", "무통장입금"),
+]
+
 
 class Payment(models.Model):
     """결재 모델"""
 
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False, blank=False)
-    payment_method = models.CharField(max_length=45)
+    payment_method = models.CharField(max_length=2, choices=PAYMENT_METHODS_CHOICES)
     payment_total_amount = models.IntegerField()
-    payment_date = models.DateTimeField()
-    payment_status = models.CharField(max_length=2, choices=PAYMENT_STATUS_CHOICES)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=2,
+        choices=PAYMENT_STATUS_CHOICES,
+        default="PC",
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         db_table = "payments"
