b89bec5c79cabc8cd273c2b88259f56ded507841
kdh92417 <kdh92417@gmail.com>
2022-09-14 19:15:52 +0900

    Style: 필요없는 import 삭제

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order
 from order.serializers import OrderSerializer
