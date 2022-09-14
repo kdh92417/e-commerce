7f42d2514dab415ecb1581a5487acb161041a02b
kdh92417 <kdh92417@gmail.com>
2022-09-14 13:40:29 +0900

    Fix: 타임 추상화 모델 수정
    create_time -> create_at 컬럼명 수정
    update_time -> updated_at 컬럼명 수정

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class TimeStampModel(models.Model):
     """타임 추상화 모델"""
 
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     class Meta:
         abstract = True
