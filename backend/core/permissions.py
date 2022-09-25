5ff412a535cbc3e1fd38bc73889e7aca636fd8be
kdh92417 <kdh92417@gmail.com>
2022-09-25 18:41:28 +0900

    :art: Feat: Order Custom Permission 추가
    오더 커스텀 권한 작성
    - 관리자는 CRUD
    - 일반유저는 본인이 등록한 주문일 때만 허용

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -22,3 +22,20 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
 
         return request.user.is_staff
+
+
+class IsAdminOrSameUser(permissions.BasePermission):
+    """
+    관리자 : CRUD
+    일반유저 : 본인이 등록한 주문에한해서만 CRUD
+    """
+
+    message = "일반유저는 본인의 주문에대해서만 수정/삭제/조회 할 수 있습니다."
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+
+        if view.action in ["retrieve", "destroy", "update", "partial_update"]:
+            return obj.user.id == request.user.id
+        return True
