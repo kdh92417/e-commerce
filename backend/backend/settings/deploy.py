de35e9ae06f5c8be0e900f3316f5b60fd9440a31
kdh92417 <kdh92417@gmail.com>
2022-09-27 16:05:48 +0900

    :wrench: Chore: 개발 DB및 컨테이너 DB설정 추가

--- backend/backend/settings/deploy.py
+++ backend/backend/settings/deploy.py
@@ -1,3 +1,19 @@
-from base import *
+from .base import *
 
 ALLOWED_HOSTS = ["*"]
+DEBUG = False
+
+"""
+Docker Container DB Information
+"""
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME"),
+        "PASSWORD": os.environ.get("DB_ROOT_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+    }
+}
+
+print("deploy: ", DATABASES)
--- backend/backend/settings/develop.py
+++ backend/backend/settings/develop.py
@@ -1,14 +1,32 @@
 from .base import *
 
+ALLOWED_HOSTS = ["*"]
+
+"""
+Docker Container DB Information
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "PASSWORD": os.environ.get("DB_ROOT_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
     }
 }
+print("develop: ", DATABASES)
+
+"""
+Local DB Information
+"""
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "ecommerce",
+#         "PASSWORD": "",
+#         "HOST": "localhost",
+#         "PORT": "3306",
+#     }
+# }
 
 DEBUG = True
