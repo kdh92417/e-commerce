f21d4b66d8caedbbc1b20758477ef334eb699fb5
kdh92417 <kdh92417@gmail.com>
2022-09-27 16:09:23 +0900

    :wrench: Chore: wsgi 설정 파일 수정
    deploy설정파일로 변경

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.develop")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.deploy")
 
 application = get_wsgi_application()
