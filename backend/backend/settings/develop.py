cdba9860c49d536c6b6b771d68ee6d8f9fc012a8
kdh92417 <kdh92417@gmail.com>
2022-09-30 14:43:12 +0900

    :wrench: Chore: 개발 데이터베이스 설정 변경

--- backend/backend/settings/develop.py
+++ backend/backend/settings/develop.py
@@ -8,25 +8,13 @@ Docker Container DB Information
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME"),
-        "PASSWORD": os.environ.get("DB_ROOT_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "NAME": "ecommerce",
+        "PASSWORD": "root",
+        "HOST": "mysql",
+        "PORT": 3306,
     }
 }
-print("develop: ", DATABASES)
 
-"""
-Local DB Information
-"""
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "ecommerce",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "3306",
-#     }
-# }
+print("develop: ", DATABASES)
 
 DEBUG = True
